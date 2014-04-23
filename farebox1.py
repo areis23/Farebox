8f201469c5be19fecbcbd7351640866a16e3e026
areis23 <aaron.reisner@gmail.com>
2014-04-23 19:17:43 -0400

    update

--- farebox1.py
+++ farebox1.py
@@ -21,7 +21,8 @@ with open('temp.txt', 'rb') as csvfile:
 		if i == num:
 			print j
 """
-
+#add up fare collected for Cumberland County
+"""
 with open('temp.txt', 'rb') as csvfile:
 	fb_reader = csv.DictReader(csvfile, delimiter=',')
 	cumberland_fare = 0.0
@@ -29,3 +30,22 @@ with open('temp.txt', 'rb') as csvfile:
 		if j['COUNTY'] == 'Cumberland':
 			cumberland_fare += float(j['FARE_COLLECTED'])
 	print cumberland_fare
+"""
+
+# create dict of counties with fare and cost totals
+with open('temp.txt', 'rb') as csvfile:
+	fb_reader = csv.DictReader(csvfile, delimiter=',')
+	counties = []
+	stats = {}
+	print "County        Fare Collected    Cost"
+	print 
+	for i in fb_reader:
+		if i['COUNTY'] not in counties:
+			counties.append(i['COUNTY'])
+			stats[i['COUNTY']] = (float(i['FARE_COLLECTED']), float(i['TOTAL_OPERATING_COST']))
+		else:
+			stats[i['COUNTY']] = (stats[i['COUNTY']][0] + float(i['FARE_COLLECTED']), stats[i['COUNTY']][1] + float(i['TOTAL_OPERATING_COST']))
+	counties.sort()
+	for item in (counties):
+		print item + ":       " + str(stats[item][0]) + "  " + str(stats[item][1])
+
